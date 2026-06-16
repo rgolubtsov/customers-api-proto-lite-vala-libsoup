c4e8f3bacb348dc765a9b0eb891b8476cdb4efc6
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2026-06-17 02:00:00 +0300

    Bump version number to 0.0.4.

--- data/sql/00-create-db-create-and-populate-table-tmp.sql
+++ data/sql/00-create-db-create-and-populate-table-tmp.sql
@@ -1,7 +1,7 @@
 --
 -- data/sql/00-create-db-create-and-populate-table-tmp.sql
 -- ============================================================================
--- Customers API Lite microservice prototype (Vala port). Version 0.0.3
+-- Customers API Lite microservice prototype (Vala port). Version 0.0.4
 -- ============================================================================
 -- A daemon written in Vala, designed and intended to be run as a microservice,
 -- implementing a special Customers API prototype with a smart yet simplified
